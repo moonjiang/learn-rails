9d0126f77bc74692f6985638b6193ee7ce4d7f75
cindy <cindy@163.com>
2016-11-28 17:41:54 +0800

    contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'visitors#new'
+         resources :contacts, only: [:new, :create]
+         root to: 'visitors#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
