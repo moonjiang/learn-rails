5410488e556c386315babee92432365e4ac4fc1a
cindy <cindy@163.com>
2016-11-29 16:18:30 +0800

    mailing list

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
          resources :contacts, only: [:new, :create]
+         resources :visitors, only: [:new, :create]
          root to: 'visitors#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
